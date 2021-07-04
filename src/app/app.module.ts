310bae100a77fb5e5eebc6d96bbd585bce24466f
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-07-04 23:17:24 +0200

    main panel disposition implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { StatsComponent } from './side-bar-components/stats/stats.component';
 import { WallsComponent } from './side-bar-components/walls/walls.component';
 import { WallsListComponent } from './side-bar-components/walls/list/list.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PostsComponent } from './posts-components/posts/posts.component';
+import { PostComponent } from './posts-components/post/post.component';
+import { PostDetailComponent } from './comments-components/post-detail/post-detail.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     TitleComponent,
     StatsComponent,
     WallsComponent,
-    WallsListComponent
+    WallsListComponent,
+    PostsComponent,
+    PostComponent,
+    PostDetailComponent
   ],
   imports: [
     BrowserModule,
