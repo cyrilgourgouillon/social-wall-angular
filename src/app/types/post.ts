091d9ec012fee477e4dfa60a482bcdfb1b81a15e
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-07-05 23:56:11 +0200

    fix post interface

--- src/app/types/post.ts
+++ src/app/types/post.ts
@@ -4,6 +4,7 @@ import { Comment } from './comment';
 export interface Post {
     id: number,
     idWall: Wall['id'],
+    titre: string,
     author: string,
     dateCreation: string,
     content: string,
