51db545d95c24fb52c63a27a2063a85facc4d4f9
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-07-05 22:53:35 +0200

    refactor components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TitleComponent } from './side-bar-components/title/title.component';
-import { StatsComponent } from './side-bar-components/stats/stats.component';
-import { WallsComponent } from './side-bar-components/walls/walls.component';
-import { WallsListComponent } from './side-bar-components/walls/list/list.component';
+import { TitleComponent } from './components/side-bar-components/title/title.component';
+import { StatsComponent } from './components/side-bar-components/stats/stats.component';
+import { WallsComponent } from './components/side-bar-components/walls/walls.component';
+import { WallsListComponent } from './components/side-bar-components/walls/list/list.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { PostsComponent } from './posts-components/posts/posts.component';
-import { PostComponent } from './posts-components/post/post.component';
-import { PostDetailComponent } from './comments-components/post-detail/post-detail.component';
+import { PostsComponent } from './components/posts-components/posts/posts.component';
+import { PostComponent } from './components/posts-components/post/post.component';
+import { PostDetailComponent } from './components/comments-components/post-detail/post-detail.component';
 
 @NgModule({
   declarations: [
